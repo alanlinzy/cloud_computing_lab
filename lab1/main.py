40265309af7dcc93738eccabcfccaaf0ef3ee724
alanlinzy <34940728+alanlinzy@users.noreply.github.com>
2020-09-19 09:10:52 -0400

    Update main.py

--- lab1/main.py
+++ lab1/main.py
@@ -53,8 +53,8 @@ def fetch_events(limit=None):
 @app.route('/index.html',methods = ['GET'])
 def root():
     print('root')
-    return render_template("index.html",user = 'back')  
-    #return send_from_directory('static','index.html')
+    #return render_template("index.html",user = 'back')  
+    return send_from_directory('static','index.html')
 
 @app.route('/events',methods = ['GET'])
 def getEvent():
