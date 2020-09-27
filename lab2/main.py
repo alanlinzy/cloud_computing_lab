462a86cf7bfd230cdb5a9efd316ff2cb79cced8e
alanlinzy <34940728+alanlinzy@users.noreply.github.com>
2020-09-26 22:27:47 -0400

    Update main.py

--- lab2/main.py
+++ lab2/main.py
@@ -60,7 +60,8 @@ def checkCookies(cookie):#unfin
     if cookie == None:
         return False
     sess_k = DS.key(USERSESS, cookie,parent=USER )
-    query = DS.query(sess_k)
+    query = DS.query()
+    query.key_filter(sess_k,'=')
     print(cookie)
     now = datetime.datetime.now()
     sess_db = list(query.fetch())
