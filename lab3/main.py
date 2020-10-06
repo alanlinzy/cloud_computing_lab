62613c753f5d6347a6da8c606b71e0ae349bbb17
alanlinzy <34940728+alanlinzy@users.noreply.github.com>
2020-10-05 23:17:01 -0400

    Update main.py

--- lab3/main.py
+++ lab3/main.py
@@ -7,30 +7,28 @@ from flask import Flask, render_template, request, jsonify,send_from_directory,r
 
 from google.cloud import datastore
 
+app = Flask(__name__)
 
-def create_app():
-    app = Flask(__name__)
+    
+app.config['SECRET_KEY'] = os.urandom(16)
 
-    app.config['SECRET_KEY'] = os.urandom(16)
+# blueprint for events routes
+from events import events as events_blueprint
+app.register_blueprint(events_blueprint)
 
-    # blueprint for events routes
-    from events import events as events_blueprint
-    app.register_blueprint(events_blueprint)
+# blueprint for auth routes
+from auth import auth as auth_blueprint
+app.register_blueprint(auth_blueprint)
 
-    # blueprint for auth routes
-    from auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint)
+# Add default route
+@app.route('/')
+def index():
+    return redirect(url_for('events'))
 
-    # Add default route
-    @app.route('/')
-    def index():
-        return redirect(url_for('events'))
-
-    return app
+return app
 
 
 
 
 if __name__ == '__main__':
-    app = create_app()
     app.run(host ='127.0.0.1',port = 8080,debug=True)
